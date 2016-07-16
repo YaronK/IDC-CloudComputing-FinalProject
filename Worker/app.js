b3be4c535ca6853f74c93b879a6c0b81bf524b92
Gilad Levy <giladlevy7@gmail.com>
2016-07-16 16:12:38 +0300

    Worker - finished Kmean + s3 part

--- Worker/app.js
+++ Worker/app.js
@@ -6,8 +6,8 @@ var
     fs = require('fs'),
     log = require('./log.js'),
     process = require('process'),
-    csv = require("fast-csv"),
-    kMeans = require('kmeans-js');
+    parse = require('csv-parse'),
+    kmeans = require('node-kmeans');
 
 var app = express();
 
@@ -19,99 +19,83 @@ var s3 = new AWS.S3({
 });
 
 app.get('/', bodyParser.json(), function (request, response) {
-    var filename = "test.csv"
-    var inputFilePath = filename
-    var params = {Key: inputFilePath};
-    output_path = "C:\\Users\\gilad\\Desktop\\" + filename;
-
-    //Get csv file from S3
-
-    var file = fs.createWriteStream(output_path);
-    file.on('close', function(){
-        console.log('done');  //prints, file created
-    });
-
-    s3.getObject(params, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else{
-        console.log("Succsess reading csv file from s3");
-        console.log(data);
-        
-        
-        };     
-    });
-    
-    s3.getObject(params).createReadStream(function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else{
-        console.log("Succsess reading csv file from s3");
-        console.log(data);
-        
-        
-        };     
-    });
-
-    s3.getObject(params).createReadStream().pipe(file);
-
-
-
-    // Read csv file from disk 
-    fs.createReadStream(output_path)
-        .pipe(csv())
-        .on("data", function(data){
-        console.log(data);
-        })
-        .on("end", function(){
-            console.log("done");
-        });
-
-    request.on('end', function () {
-        response.sendStatus(200);
-    });
-    
-  
-
+    var kNum = 3;
+    var inputFilePath = "datasets/iris.csv"
+    initateKMeans(inputFilePath, kNum, response);
 });
 
 app.post('/', bodyParser.json(), function (request, response) {
-    console.log("Received message, body:" + JSON.stringify(request.body));
-
-    // var fileName = request.body.fileName,
-    //    inputFilePath = "input/" + fileName,
-    //    outputFilePath = "output/" + fileName,
-    //    tempFilePath = "/tmp/" + fileName;
-
-    var filename = "test.csv"
-
-    s3.getObject({ Key: inputFilePath }, function (err, data) {
-        if (err) { log(err); return; }
-
-            console.log(data)
-            /** 
-            var data = [[1, 2, 3], [69, 10, 25]];
-            var km = new kMeans({
-                K: 2
-            });
-
-            km.cluster(data);
-            while (km.step()) {
-                km.findClosestCentroids();
-                km.moveCentroids();
-
-                console.log(km.centroids);
-
-                if(km.hasConverged()) break;
-            }
-
-            console.log('Finished in:', km.currentIteration, ' iterations');
-            console.log(km.centroids, km.clusters);
-            */
-        s3.deleteObject({ Key: inputFilePath }, function (err) { if (err) log(err); });
-    });
-
-    request.on('end', function () {
-        response.sendStatus(200);
-    });
+    var inputFilePath = "datasets/" + request.body.dataset;
+    var kNum = request.body.params.kNum;
+    if (request.method.toString() == "KMean") {
+        initateKMeans(inputFilePath, kNum, response);
+    };
 });
+
+function initateKMeans(inputFilePath, kNum, response) {
+    
+    //Initiate parameters
+    inputFilePath = inputFilePath.toString();
+    var params = { Key: inputFilePath };
+    outputName = inputFilePath.replace('csv', '')
+    outputName = outputName.replace('datasets/', 'results/')
+    var output_path = outputName + "KMean." + kNum + ".csv";
+
+    // Get data from S3
+    s3.getObject(params, function (err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else {
+            console.log("Succeeded reading csv file from s3");
+
+            // Parse data into a csv format
+            parse(data.Body.toString(), {}, function (err, output) {
+                firstRow = output.shift();
+
+                // Call KMean clustering function
+                kMeanClustering(output, firstRow, kNum, output_path, response);
+            });
+        };
+    });
+}
+function kMeanClustering(data, firstRow, kNum, output_path, response) {
+    var vectors = data;
+    var result = data;
+
+    // K Mean Clustering
+    kmeans.clusterize(vectors, { k: kNum }, (err, res) => {
+        if (err) console.error(err);
+        else {
+            for (var i = 0; i < res.length; i++) {
+                clusterIds = res[i].clusterInd;
+                for (var j = 0; j < clusterIds.length; j++) {
+                    clusterId = clusterIds[j];
+                    var clusterName = "Cluster " + (i).toString();
+                    result[clusterId].push(clusterName);
+                };
+            };
+            // Create first row of parameters
+            firstRow.push("clusterIndex");
+            result.unshift(firstRow);
+
+            // Parse results into a csv format
+            var lineArray = [];
+            result.forEach(function (infoArray, index) {
+                var line = infoArray.join(",");
+                lineArray.push(index == 0 ? "" + line : line);
+            });
+            var csvContent = lineArray.join("\n");
+
+            // Uplocad csv to S3
+            var uploadParams = {
+                Key: output_path,
+                Body: csvContent
+            };
+            s3.putObject(uploadParams, function (err, data) {
+                console.log("Successfully uploaded csv to s3");
+                response.status(200).end();
+            });
+        }
+    });
+}
 
 app.listen(process.env.PORT || '3000');
