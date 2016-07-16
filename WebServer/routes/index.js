f14778f6557bc27fcd895e929f1036d0a9443a98
yakan <yakan@microsoft.com>
2016-07-16 16:00:17 +0300

    single-result / all-results, cluster-dataset queues sqs message

--- WebServer/routes/index.js
+++ WebServer/routes/index.js
@@ -18,51 +18,84 @@ var s3 = new AWS.S3({
 var s3bucketHref = s3.endpoint.href + config.aws.s3.bucket + "/";
 var sqs = new AWS.SQS({ params: { QueueUrl: config.aws.sqs.queueUrl } });
 
+var outputS3Prefix = "output/";
+
 function renderError(res, err) { res.render('error', { message: err.message, error: err }); }
-function renderIndex(res, message) {
-  s3.listObjects({ Prefix: "output/" }, function (err, data) {
-    if (err) log(err);
+function renderAllResults(res) {
+  s3.listObjects({ Prefix: outputS3Prefix }, function (err, data) {
+    if (err) { log(err); renderError(res, err); }
     else {
       var keys = data.Contents.map(function (object) { return object.Key; });
-      keys.splice(keys.indexOf('output/'), 1);
-      var urls = keys.map(function (key) { return s3bucketHref + key; });
-      res.render('index', { hrefs: urls, message: message });
+      keys.splice(keys.indexOf(outputS3Prefix), 1);
+      var resultIds = keys.map(function (key) { return key.substring(outputS3Prefix.length) });
+      res.render('all-results', { resultIds: resultIds });
     }
   });
 }
 
 router.get('/', function (req, res, next) {
-  renderIndex(res, "");
+  renderAllResults(res);
 });
 
-router.post('/', upload.single('image'), function (req, res, next) {
-  if (!req.file) { renderIndex(res, "Please select an image first."); return; }
+router.get('/all-results', function (req, res, next) {
+  renderAllResults(res);
+});
 
-  var localFilePath = req.file.path;
-  var remoteFileName = req.file.filename + '.' + req.file.originalname.split('.').pop();
-  var remoteFilePath = "input/" + remoteFileName;
+router.get('/single-result/:resultId', function (req, res, next) {
+  res.render('single-result', {
+    resultId: req.params.resultId,
+    resultLink: s3bucketHref + outputS3Prefix + req.params.resultId
+  });
+});
 
-  s3.upload({ Key: remoteFilePath, Body: fs.createReadStream(localFilePath) }).
-    send(function (err, data) {
+router.get('/cluster-dataset', function (req, res, next) {
+  res.render('cluster-dataset', { dataset_names: ["1", "2"] });
+});
+
+router.post('/cluster-dataset', function (req, res, next) {
+  var clusteringMethod = req.body["clustering-method"];
+  var datasetName = req.body["dataset-name"];
+  var kMeansK = req.body["k-means-k"];
+  sqs.sendMessage(
+    {
+      MessageBody: JSON.stringify({
+        dataset: datasetName,
+        method: clusteringMethod,
+        params: {
+          kNum: kMeansK
+        },
+      })
+    },
+    function (err, data) {
       if (err != null) { renderError(res, err); }
-      else {
-        fs.unlink(localFilePath);
-        sqs.sendMessage(
-          { MessageBody: JSON.stringify({ fileName: remoteFileName }) },
-          function (err, data) {
-            if (err != null) { renderError(res, err); }
-            else { renderIndex(res, req.file.originalname + " uploaded successfully") }
-          });
-      }
+      else { res.render('cluster-dataset', { message: "Queued clustering." }); }
     });
 });
 
-router.get('/cluster-dataset', function(req, res, next) {
-  res.render('cluster-dataset', {dataset_names: ["1", "2"]});
-});
-
-router.get('/upload-dataset', function(req, res, next) {
+router.get('/upload-dataset', function (req, res, next) {
   res.render('upload-dataset');
 });
+
+//router.post('/', upload.single('image'), function (req, res, next) {
+//  if (!req.file) { renderIndex(res, "Please select an image first."); return; }
+//
+//  var localFilePath = req.file.path;
+//  var remoteFileName = req.file.filename + '.' + req.file.originalname.split('.').pop();
+//  var remoteFilePath = "input/" + remoteFileName;
+//
+//  s3.upload({ Key: remoteFilePath, Body: fs.createReadStream(localFilePath) }).
+//    send(function (err, data) {
+//      if (err != null) { renderError(res, err); }
+//      else {
+//        fs.unlink(localFilePath);
+//        sqs.sendMessage(
+//          { MessageBody: JSON.stringify({ fileName: remoteFileName }) },
+//          function (err, data) {
+//            if (err != null) { renderError(res, err); }
+//            else { renderIndex(res, req.file.originalname + " uploaded successfully") }
+//          });
+//      }
+//    });
+//});
 
 module.exports = router;
